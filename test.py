32cb271e8535e237b88bd9ba3243b6eb85f19a5b
kanye <wille_ost@protonmail.com>
2023-03-17 21:23:41 +0800

    fix bug

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import pyautogui
 import time
 
-count = 0
 while True:
     buttonLocation = pyautogui.locateOnScreen('target.png', confidence=.7)
     if buttonLocation != None:
         pyautogui.click(pyautogui.center(buttonLocation))
         time.sleep(5)
-        count += 1
-        print("Click times: " + count)
     time.sleep(1)
